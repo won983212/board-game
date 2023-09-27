8f9945b363ffa6f750419c29b85762f50e839cce
hyun.2 <hyun.2@navercorp.com>
2023-09-27 15:07:29 +0900

    Initial commit

---
+++ src/test/java/com/won983212/boardgame/BoardGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.won983212.boardgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
