472776e3ccc2c7ba09441bede48bc62d056954ea
won983212 <won983212@naver.com>
2023-09-29 12:47:58 +0900

    test: 테스트 데이터 넣기

--- src/main/java/com/won983212/boardgame/domain/player/repository/PlayerRepository.java
+++ src/main/java/com/won983212/boardgame/domain/player/repository/PlayerRepository.java
@@ -10,16 +10,20 @@ import com.won983212.boardgame.domain.player.model.Player;
 
 @Repository
 public class PlayerRepository {
-	// TODO redis로 교체: 동시성 이슈, 데이터 안정성, 확장 가능성
-	private final Map<Long, Player> players = new HashMap<>();
-	private static long nextId = 1;
+    // TODO redis로 교체: 동시성 이슈, 데이터 안정성, 확장 가능성
+    // TODO Test data
+    private final Map<Long, Player> players = new HashMap<>(Map.of(
+            1L, Player.of("hello"),
+            2L, Player.of("hello2")
+    ));
+    private static long nextId = 3;
 
-	public Optional<Player> findById(Long id) {
-		return Optional.ofNullable(players.get(id));
-	}
+    public Optional<Player> findById(Long id) {
+        return Optional.ofNullable(players.get(id));
+    }
 
-	public void save(Player player) {
-		long id = nextId++;
-		players.put(id, player.withId(id));
-	}
+    public void save(Player player) {
+        long id = nextId++;
+        players.put(id, player.withId(id));
+    }
 }
--- src/main/java/com/won983212/boardgame/domain/room/repository/RoomRepository.java
+++ src/main/java/com/won983212/boardgame/domain/room/repository/RoomRepository.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.won983212.boardgame.domain.game.model.GameType;
 import org.springframework.stereotype.Repository;
 
 import com.won983212.boardgame.domain.room.model.Room;
@@ -11,8 +12,12 @@ import com.won983212.boardgame.domain.room.model.Room;
 @Repository
 public class RoomRepository {
 	// TODO redis로 교체: 동시성 이슈, 데이터 안정성, 확장 가능성
-	private final Map<Long, Room> rooms = new HashMap<>();
-	private static long nextRoomId = 1;
+	// TODO Test data
+	private final Map<Long, Room> rooms = new HashMap<>(Map.of(
+			1L, Room.of("초보만", GameType.OMOK, 1L),
+			2L, Room.of("안녕하세요", GameType.OMOK, 2L)
+	));
+	private static long nextRoomId = 3;
 
 	public Collection<Room> findAllRooms() {
 		return rooms.values();
