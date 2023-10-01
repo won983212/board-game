718cae86cdd597fe36de0c03c0cab8c8e134a93b
won983212 <won983212@naver.com>
2023-10-01 12:53:56 +0900

    feat: 로그인 폼

---
+++ src/main/java/com/won983212/boardgame/domain/controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.won983212.boardgame.domain.controller;
+
+import com.won983212.boardgame.domain.game.enums.GameType;
+import com.won983212.boardgame.domain.player.dto.LoginRequest;
+import com.won983212.boardgame.domain.room.dto.CreateRoomRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+@RequiredArgsConstructor
+public class LoginController {
+
+    @GetMapping
+    public String login(Model model) {
+        model.addAttribute("loginRequest", new LoginRequest(""));
+        return "login";
+    }
+
+    @PostMapping
+    public void doLogin(@ModelAttribute LoginRequest loginRequest) {
+
+    }
+}
---
+++ src/main/java/com/won983212/boardgame/domain/player/dto/LoginRequest.java
@@ -0,0 +1,10 @@
+package com.won983212.boardgame.domain.player.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class LoginRequest {
+    private final String username;
+}
