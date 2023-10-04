3a5ee8b2adfefd907bd20e3b7a6277f7f63ca6a9
won983212 <won983212@naver.com>
2023-10-04 19:15:02 +0900

    feat: 오목 html 베이스 구현

--- src/main/resources/static/js/omok.js
+++ src/main/resources/static/js/omok.js
@@ -1,6 +1,9 @@
 // socket
 const sock = new SockJS('http://localhost:8080/ws/game');
 
+// modal
+const gameFinishModal = new bootstrap.Modal(document.getElementById('gameFinishModal'))
+
 // canvas
 const canvas = document.getElementById("gamePanel");
 const ctx = canvas.getContext('2d');
@@ -8,6 +11,38 @@ const ctx = canvas.getContext('2d');
 // constants
 const width = canvas.width;
 const height = canvas.height;
+
+// draw constants
+const lineOffset = 10;
+const lineCount = 19;
+const sqWidth = (width - lineOffset * 2) / (lineCount - 1);
+const sqHeight = (height - lineOffset * 2) / (lineCount - 1);
+const stoneRadius = Math.min(sqWidth, sqHeight) / 2 - 1
+
+// context
+let stones = [
+    {
+        color: 'black',
+        x: 1,
+        y: 3
+    },
+    {
+        color: 'black',
+        x: 2,
+        y: 3
+    },
+    {
+        color: 'white',
+        x: 5,
+        y: 5
+    },
+    {
+        color: 'white',
+        x: 6,
+        y: 6
+    }
+];
+
 
 sock.onopen = function () {
     console.log('Connected!')
@@ -26,11 +61,6 @@ sock.onclose = function () {
     console.log('close')
 }
 
-function initializeCanvas() {
-    ctx.fillStyle = '#CC8954'
-    ctx.fillRect(0, 0, width, height)
-}
-
 function send(type, data) {
     sock.send(JSON.stringify({
         type: type,
@@ -38,4 +68,41 @@ function send(type, data) {
     }))
 }
 
-initializeCanvas()
+function renderCanvas() {
+    ctx.fillStyle = '#CC8954'
+    ctx.fillRect(0, 0, width, height)
+
+    renderLines()
+    renderStones()
+}
+
+function renderLines() {
+    ctx.strokeStyle = '#000000'
+
+    for (let x = 0; x < lineCount; x++) {
+        ctx.beginPath()
+        ctx.moveTo(lineOffset + x * sqWidth, lineOffset)
+        ctx.lineTo(lineOffset + x * sqWidth, height - lineOffset)
+        ctx.stroke()
+    }
+
+    for (let y = 0; y < lineCount; y++) {
+        ctx.beginPath()
+        ctx.moveTo(lineOffset, lineOffset + y * sqHeight)
+        ctx.lineTo(width - lineOffset, lineOffset + y * sqHeight)
+        ctx.stroke()
+    }
+}
+
+function renderStones() {
+    for (const stone of stones) {
+        ctx.beginPath()
+        ctx.fillStyle = stone.color;
+        ctx.arc(lineOffset + stone.x * sqWidth,
+            lineOffset + stone.y * sqHeight, stoneRadius,
+            0, 2 * Math.PI);
+        ctx.fill();
+    }
+}
+
+renderCanvas()
